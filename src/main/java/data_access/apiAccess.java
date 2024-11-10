1a942a395f961d4b056ac4094f2b317c02a3e643
beam <108953955+bem-boi@users.noreply.github.com>
2024-11-10 05:12:46 -0500

    implemented api access

---
+++ src/main/java/data_access/apiAccess.java
@@ -0,0 +1,49 @@
+package data_access;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public final class apiAccess {  // make this a static class later and package it
+
+    private static final String API_KEY = "baw+70SpL57JbD/nNLXgOQ==RTGa7BsSwkAMlsBQ";
+    private static final String API_URL = "https://api.api-ninjas.com/v1/animals?name=";
+
+    private apiAccess(){
+        throw new AssertionError("Instantiating utility class.");
+    }
+
+    public static String getAnimal(String animal){  // returns a list of animals with the keyword 'animal' in it
+        String apiUrl = API_URL + animal;
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("X-Api-Key", API_KEY);
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                return response.toString();
+            } else {
+                return "Error: " + responseCode + " " + connection.getResponseMessage();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(apiAccess.getAnimal("cheetah"));
+
+    }
+}
