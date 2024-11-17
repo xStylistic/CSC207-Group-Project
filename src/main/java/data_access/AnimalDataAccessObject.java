c078bd824823487bb98211b7ded73551da764a5e
beam <108953955+bem-boi@users.noreply.github.com>
2024-11-17 18:25:59 -0500

    able to add fun fact n stuff now and renamed it

--- src/main/java/data_access/AnimalDataAccessObject.java
+++ src/main/java/data_access/AnimalDataAccessObject.java
@@ -1,18 +1,26 @@
 package data_access;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * API access file.
  */
-public final class ApiAccess {
+public final class AnimalDataAccessObject {
 
     private static final String API_KEY = "baw+70SpL57JbD/nNLXgOQ==RTGa7BsSwkAMlsBQ";
     private static final String API_URL = "https://api.api-ninjas.com/v1/animals?name=";
@@ -31,43 +39,8 @@ public final class ApiAccess {
     );
     private static final Map<String, List> CURRENT_ANIMALS = new HashMap<>();
 
-    private ApiAccess() {
+    private AnimalDataAccessObject() {
         throw new AssertionError("Instantiating utility class.");
-    }
-
-    /**
-     * Gets all the data given the animal.
-     * @param animal is animal name
-     * @return string of animals and their data
-     */
-    public static String getData(String animal) {
-        final String apiUrl = API_URL + animal;
-        try {
-            final URL url = new URL(apiUrl);
-            final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("X-Api-Key", API_KEY);
-
-            final int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                final BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String inputLine;
-                final StringBuilder response = new StringBuilder();
-
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-                return response.toString();
-            }
-            else {
-                return "Error: " + responseCode + " " + connection.getResponseMessage();
-            }
-        }
-        catch (Exception exA) {
-            exA.printStackTrace();
-        }
-        return null;
     }
 
     /**
@@ -77,12 +50,12 @@ public final class ApiAccess {
      */
     public static List getAnimal(String animal) {
         final String apiUrl = API_URL + animal;
+        final List<String> animals = new ArrayList<>();
         try {
             final URL url = new URL(apiUrl);
             final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("X-Api-Key", API_KEY);
-            final List<String> animals = new ArrayList<>();
 
             final int responseCode = connection.getResponseCode();
             if (responseCode == HttpURLConnection.HTTP_OK) {
@@ -99,37 +72,46 @@ public final class ApiAccess {
                 for (int i = 0; i < jsonArray.length(); i++) {
                     final JSONObject jsonObject = jsonArray.getJSONObject(i);
                     final JSONObject taxonomy = jsonObject.getJSONObject("taxonomy");
-//                    final JSONObject characteristics = jsonObject.getJSONObject("characteristics");
+                    final JSONObject characteristics = jsonObject.getJSONObject("characteristics");
                     // check if it's in the same family because there might be other animals with same name but
                     // different species
+                    final Iterator<String> keys = characteristics.keys();
+                    final String[] keyArray = new String[characteristics.length()];
+                    int index = 0;
+                    while (keys.hasNext()) {
+                        keyArray[index++] = keys.next();
+                    }
+                    final String randomKey = keyArray[new Random().nextInt(keyArray.length)];
                     System.out.println(jsonObject.getJSONArray("locations"));
                     if (taxonomy.getString("family").equals(AVAILABLE_ANIMALS.get(animal))) {
                         animals.add(jsonObject.getString("name"));
                         CURRENT_ANIMALS.put(jsonObject.getString("name"), new ArrayList<>(Arrays.asList(
                                 jsonObject.getJSONArray("locations"),
-//                                characteristics.getString("most_distinctive_feature"),
+                                randomKey + ": " + characteristics.getString(randomKey),
                                 taxonomy.getString("family")
                         )));
                     }
                 }
-                return animals;
-            }
-            else {
-                return null;
             }
         }
-        catch (Exception exA) {
+        catch (IOException exA) {
             exA.printStackTrace();
         }
-        return null;
+        catch (JSONException exA) {
+            exA.printStackTrace();
+        }
+        return animals;
     }
 
     /**
-     * Main test.
+     * The main entry point of the application.
+     * <p>
+     * Runs the program to just test it
+     * </p>
      * @param args commandline arguments are ignored
      */
     public static void main(String[] args) {
-        System.out.println(ApiAccess.getAnimal("pig"));
-
+        System.out.println(AnimalDataAccessObject.getAnimal("pig"));
+        System.out.println(CURRENT_ANIMALS);
     }
 }
