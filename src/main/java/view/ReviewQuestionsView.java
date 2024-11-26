9607d18cbeb932872091da72377d3dcbc4ef84e0
Bonny Chen <70673137+xStylistic@users.noreply.github.com>
2024-11-25 19:29:04 -0500

    fixed ReviewQuestionsView

--- src/main/java/view/ReviewQuestionsView.java
+++ src/main/java/view/ReviewQuestionsView.java
@@ -1,7 +1,4 @@
-package view;/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+package view;
 
 /**
  *
