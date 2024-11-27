6380a5acf4b04963d260f583042c0fad2514c1e0
beam <108953955+bem-boi@users.noreply.github.com>
2024-11-26 23:59:52 -0500

    made animalfarm simpler and made it all a list and also modified the functions accordingly

--- src/main/java/entity/AnimalFarm.java
+++ src/main/java/entity/AnimalFarm.java
@@ -8,17 +8,15 @@ import data_access.AnimalDataAccessObject;
  * The animals in the program.
  */
 public class AnimalFarm {
-    private Map<String, Integer> currentAnimals;
+    private List<Animal> currentAnimals;
     private List<Animal> selectedAnimals;
-    private Map<String, Animal> animalMap;
     private Random rand = new Random();
     private List<String> availableAnimals;
 
     public AnimalFarm(String[] availableAnimals) {
         this.availableAnimals = Arrays.asList(availableAnimals);
         this.selectedAnimals = new ArrayList<>();
-        this.currentAnimals = new HashMap<>();
-        this.animalMap = new HashMap<>();
+        this.currentAnimals = new ArrayList<Animal>();
 
         for (String animalsSpecies : this.availableAnimals) {
             final Map<String, List> listOfAnimalSameSpecies =
@@ -31,7 +29,6 @@ public class AnimalFarm {
                             (String) listOfAnimalSameSpecies.get(animalName).get(1));
 
                     selectedAnimals.add(tempAnimal);
-                    animalMap.put(tempAnimal.getName(), tempAnimal);
                 }
                 else {
                     System.err.println("Animal not found or family does not match: " + animalName);
@@ -45,12 +42,10 @@ public class AnimalFarm {
      */
     public void addAnimal() {
         if (!selectedAnimals.isEmpty()) {
-            int index = rand.nextInt(selectedAnimals.size());
-            Animal animalToAdd = selectedAnimals.get(index);
-            String name = animalToAdd.getName();
-
+            final int index = rand.nextInt(selectedAnimals.size());
+            final Animal animalToAdd = selectedAnimals.get(index);
             // Increase the number of the animal
-            currentAnimals.put(name, currentAnimals.getOrDefault(name, 0) + 1);
+            currentAnimals.add(animalToAdd);
         }
     }
 
@@ -61,29 +56,14 @@ public class AnimalFarm {
     public void removeAnimal(int removeCount) {
         if (!currentAnimals.isEmpty()) {
             for (int i = 0; i < removeCount; i++) {
-                // Get a list of animal names currently on the farm
-                List<String> animalNames = new ArrayList<>(currentAnimals.keySet());
-                int index = rand.nextInt(animalNames.size());
-                String name = animalNames.get(index);
-
-                // Decrease the count of the animal
-                int count = currentAnimals.get(name);
-                if (count > 1) {
-                    currentAnimals.put(name, count - 1);
-                }
-                else {
-                    currentAnimals.remove(name);
-                }
+                final int randomInt = rand.nextInt(currentAnimals.size());
+                currentAnimals.remove(randomInt);
             }
         }
     }
 
     public List<Animal> getSelectedAnimals() {
         return selectedAnimals;
-    }
-
-    public Map<String, Integer> getCurrentAnimals() {
-        return currentAnimals;
     }
 
     public Map<String, Animal> getAnimalMap() {
