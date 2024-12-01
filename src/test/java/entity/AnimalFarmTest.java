ab0c99d2634cb6779022a6389f871bfdbd4f317d
beam <108953955+bem-boi@users.noreply.github.com>
2024-12-01 16:51:58 -0500

    did testing and eh uml diagram

--- src/test/java/entity/AnimalFarmTest.java
+++ src/test/java/entity/AnimalFarmTest.java
@@ -1,4 +1,49 @@
 package entity;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class AnimalFarmTest {
+    @Test
+    public void testAddAnimal() {
+        String[] animals = {
+                "pig",
+                "alpaca",
+                "horse",
+                "cow",
+                "chicken",
+                "fox",
+                "bear",
+                "tiger",
+                "flamingo",
+                "rabbit",
+        };
+        AnimalFarm farm = new AnimalFarm(animals);
+        assertEquals(0, farm.getCurrentAnimals().size());
+        farm.addAnimal();
+        assertEquals(1, farm.getCurrentAnimals().size());
+    }
+
+    @Test
+    public void testRemoveAnimal() {
+        String[] animals = {
+                "pig",
+                "alpaca",
+                "horse",
+                "cow",
+                "chicken",
+                "fox",
+                "bear",
+                "tiger",
+                "flamingo",
+                "rabbit",
+        };
+        AnimalFarm farm = new AnimalFarm(animals);
+        assertEquals(0, farm.getCurrentAnimals().size());
+        farm.addAnimal();
+        assertEquals(1, farm.getCurrentAnimals().size());
+        farm.removeAnimal(1);
+        assertEquals(0, farm.getCurrentAnimals().size());
+    }
 }
--- src/test/java/use_case/animal/AnimalDataAccessTest.java
+++ src/test/java/use_case/animal/AnimalDataAccessTest.java
@@ -3,9 +3,7 @@ package use_case.animal;
 import data_access.AnimalDataAccessObject;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -14,16 +12,20 @@ public class AnimalDataAccessTest {
     @Test
     public void testAnimalList() {
         Map<String, List<Object>> recievedAnimals = AnimalDataAccessObject.getAnimal("tiger");
-        String[] animals = {"Bengal Tiger", "Indochinese Tiger", "Malayan Tiger", "Saber-Toothed Tiger",
-                "Siberian Tiger", "South China Tiger", "Sumatran Tiger", "Tiger", "White Tiger"};
+        String[] animals = {"Sumatran Tiger", "White Tiger", "Indochinese Tiger", "Saber-Toothed Tiger",
+                "Malayan Tiger", "South China Tiger", "Bengal Tiger", "Tiger", "Siberian Tiger"};
         List<String> expectedAnimals = Arrays.asList(animals);
-        assertEquals(recievedAnimals, expectedAnimals);
+        List<String> realAnimals = new ArrayList();
+        for (String animal: recievedAnimals.keySet()) {
+            realAnimals.add(animal);
+        }
+        assertEquals(expectedAnimals, realAnimals);
     }
 
     @Test
     public void testAnimalLocation() {
         Map<String, List<Object>> recievedAnimals = AnimalDataAccessObject.getAnimal("pig");
-        List<String> location = (List) recievedAnimals.get("pig").get(1);
+        List<String> location = (List) recievedAnimals.get("Pig").get(0);
         String[] locationPig = {"Asia","Eurasia","Europe","North-America"};
         List<String> expectedLocation = Arrays.asList(locationPig);
         assertEquals(location, expectedLocation);
@@ -32,7 +34,7 @@ public class AnimalDataAccessTest {
     @Test
     public void animalNotInAvailableAnimals() {
         Map<String, List<Object>> recievedAnimals = AnimalDataAccessObject.getAnimal("cat");
-        assertNull(recievedAnimals);
+        assertEquals(new HashMap<String, List<Object>>(), recievedAnimals);
     }
 
 }
