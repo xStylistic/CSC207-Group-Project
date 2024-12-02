ac59f8dac536b2c1c136a00355f9f15c13402e78
SazzGuy <saswatamoh@gmail.com>
2024-12-02 00:17:23 -0500

    fixed checkstyle in view_manager

--- src/main/java/data_access/Constants.java
+++ src/main/java/data_access/Constants.java
@@ -9,4 +9,6 @@ public class Constants {
     public static final int FOURFORTY = 440;
     public static final int TEN = 10;
     public static final int THOUSAND = 1000;
+    public static final int NINEHUNDREDTWENTYSEVEN = 927;
+    public static final int FIVEHUNDREDNINETYONE = 591;
 }
