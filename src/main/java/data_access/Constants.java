d9cec18f6ede7e2438475474b33446c5ee693cfe
SazzGuy <saswatamoh@gmail.com>
2024-12-01 20:19:36 -0500

    created constants file and fixed checkstyle errors in animal

---
+++ src/main/java/data_access/Constants.java
@@ -0,0 +1,9 @@
+package data_access;
+
+public class Constants {
+    public static final int HUNDRED = 100;
+    public static final int SEVENHUNDREDTWENTYTHREE = 723;
+    public static final int FOURFORTY = 440;
+    public static final int TEN = 10;
+
+}
